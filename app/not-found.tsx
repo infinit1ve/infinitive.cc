9442d7a65a7662e977a44ec88edf420454cfafb6
dusky <contact@infinitive.cc>
2026-02-20 11:03:36 +0100

    feat: update Next.js configuration and add NotFound page
    - Set output to "export" in next.config.ts for static export
    - Enable unoptimized images for next/image in static export
    - Add trailing slash support in next.config.ts
    - Create a new NotFound page with a hyperlink to return to the previous page
    - Add Open Graph image (og.png) to the public directory

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Hyperlink from "./components/hyperlink";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-5 justify-center self-center mt-auto text-center p-10">
+      <h1 className="text-3xl">404</h1>
+      <p>
+        Page not found. You can{" "}
+        <Hyperlink name="return to the previous page" url="../" />
+      </p>
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true }, // required for next/image on static export
+  trailingSlash: true,
 };
 
 export default nextConfig;
