2e476d303c67edca7e0825f6d1f2bbec08870d1f
Max <contact@infinitive.cc>
2026-04-28 21:23:07 +0200

    Added a schema.org person schema for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import Footer from "./components/footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://infinitive.cc"),
   title: {
-    default: "infinitive",
+    default: "Maksym",
     template: "%s | infinitive",
   },
-  description: "Running my own stack on the internet.",
+  description:
+    "Running my own stack on the internet. Portfolio of Maksym Tarashchan.",
   creator: "Maksym Tarashchan",
   publisher: "Maksym Tarashchan",
   openGraph: {
     type: "website",
     url: "https://infinitive.cc",
     siteName: "infinitive.cc",
-    title: "infinitive.cc",
+    title: "infinitive Portfolio",
     description: "Running my own stack on the internet.",
     images: [
       {
@@ -40,14 +42,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const personSchema = {
+    "@context": "https://schema.org/",
+    "@type": "Person",
+    name: "Maksym Tarashchan",
+    url: "https://infinitive.cc",
+    sameAs: [
+      "https://github.com/infinit1ve",
+      "https://linkedin.com/in/maksym-tarashchan/",
+      "https://www.instagram.com/m.taraschan/",
+    ],
+  };
   return (
     <html lang="en">
+      <Script
+        id="person-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
+      />
       <body>
         <div className="max-w-3xl w-full min-h-screen flex flex-col">
           {children}
           <Footer />
         </div>
-        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <script
+          async
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
       </body>
     </html>
   );
